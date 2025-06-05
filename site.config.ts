be77c74d2a81f43f379c46165bd0780901bf12b8
shivprasad <shivprasad@gmail.com>
2025-06-04 21:24:43 -0700

    Updated: site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,24 +2,24 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: '69ea3f9851e5426fb64320364aa276ae',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Shivprasad Bade - Life is but a dream',
+  domain: 'bytecode-seven.vercel.app',
+  author: 'Shivprasad Bade',
 
   // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
+  description: "Welcome to 'Life is but a dream' - a blog where I share my journey as a developer. It's a mix of coding insights, personal experiences, and the little epiphanies that come with a life in tech. Whether you're deep in the world of development or just curious about it, join me in unraveling the simplicity and complexity of coding and how it shapes our digital world.",
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  twitter: 'shivprasad',
+  github: 'shivprasad',
+  linkedin: 'shivprasadb',
   // mastodon: '#', // optional mastodon profile URL, provides link verification
   // newsletter: '#', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
